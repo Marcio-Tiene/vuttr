ea8371552137636843e126f0de980c04cb942077
Marcio Tiene <marciorft@gmail.com>
2020-11-20 18:02:54 -0300

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Globalstyle from './GolbalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Globalstyle />
   </React.StrictMode>,
   document.getElementById('root')
 );
