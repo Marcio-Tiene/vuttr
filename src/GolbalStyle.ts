6006d832ae6448af34107146df426f47c5fef005
Marcio Tiene <64861933+Marcio-Tiene@users.noreply.github.com>
2020-11-23 10:15:37 -0300

    typografy and some default colors added (#6)

--- src/GolbalStyle.ts
+++ src/GolbalStyle.ts
@@ -4,8 +4,8 @@ const Globalstyle = createGlobalStyle`
 
 --DarkestWhite: #EBEAED;
 --DarkerWhite: #F5F4F6;
---Yellow: #FFBB43;
-
+--Ink: #170C3A;
+--MosDarkestWhite: #DEDCE1;
 
 *{
     
@@ -15,11 +15,7 @@ const Globalstyle = createGlobalStyle`
 }
 html {
   
-  font-family: 'Source Sans Pro', sans-serif;
-  
-
-
-  
+  font-family: 'Source Sans Pro', sans-serif;  
 }
 
 body{
@@ -30,19 +26,37 @@ body{
 align-items: center;
 justify-content: center;
   
-  background-color:#6554C0;
-  
- 
-  
-  
-  
-
+  background-color: var(--DarkerWhite);
 }
+
 h1, h2, h3, h4, h5, h6{
   font-weight: 600;
-  color: #FFBB43;
+  color: #170C3A; 
+}
+
+h1 {
+  font-size:2.625rem;
+
+}
+h2{
+  font-size:2.25rem;
+  }
   
-  
+h3{
+  font-size:1.875rem;
+}
+
+h4{
+  font-size:1.625rem,
+}
+
+h5{
+  font-size:1.5rem;
+}
+
+p {
+  font-weight: normal;
+  font-size:1.25rem;
 }
 `;
 
