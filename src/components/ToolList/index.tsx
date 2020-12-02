0a5e843f6dfa37f3c127816ccaec379ad2c725c4
Marcio Tiene <marciorft@gmail.com>
2020-12-02 17:25:11 -0300

    delete tools implemented

--- src/components/ToolList/index.tsx
+++ src/components/ToolList/index.tsx
@@ -4,11 +4,17 @@ import ToolListGlobalState from '../../hooks/ToolLlistGlobalState';
 import ToolCard from '../ToolCard';
 import { ToolListContainer } from './styles';
 import { ITools } from '../../interfaces/ITools';
+import api from '../../services/api';
 
 const ToolList: React.FC = () => {
   const { LoadAllTools } = new ToolsRepository();
 
   const { toolList, setToolList } = ToolListGlobalState();
+
+  async function removeTool(tool: ITools) {
+    await api.delete(`/tools/${tool.id}`);
+    setToolList(await LoadAllTools());
+  }
 
   useEffect(() => {
     (async () => {
@@ -21,6 +27,7 @@ const ToolList: React.FC = () => {
     <ToolListContainer>
       {toolList.map((tool) => (
         <ToolCard
+          removeButtonOnClick={() => removeTool(tool)}
           key={`${tool.id}${tool.title}`}
           id={tool.id}
           description={tool.description}
