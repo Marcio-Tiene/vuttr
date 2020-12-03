aecda5ec4fcd4846479757978539f63469079f22
Marcio Tiene <marciorft@gmail.com>
2020-12-02 21:02:12 -0300

    create delete  warning

---
+++ src/components/DeleteToolModal/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ModalBackground } from '../AddToolFormModal/styles';
+
+const DeleteToolModal: React.FC = () => {
+  return <ModalBackground open={true}></ModalBackground>;
+};
+
+export default DeleteToolModal;
