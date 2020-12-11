29d7510e6f25925db246d7ef85730f695cda95e1
Marcio Tiene <marciorft@gmail.com>
2020-12-10 22:56:16 -0300

    Logo responsivness added

--- src/components/Title/styles.tsx
+++ src/components/Title/styles.tsx
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 export const LogoImg = styled.img`
   height: 64px;
 
-  overflow: hidden;
+  @media (max-width: 700px) {
+    width: 100%;
+  }
 `;
 
 export const Subtitle = styled.h2`
