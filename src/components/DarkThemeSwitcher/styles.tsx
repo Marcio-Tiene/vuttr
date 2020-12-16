c0f680f069334f4dd9409a66236240e15c0d771c
Marcio Tiene <marciorft@gmail.com>
2020-12-16 16:58:22 -0300

    fixing darkmode switch on top

--- src/components/DarkThemeSwitcher/styles.tsx
+++ src/components/DarkThemeSwitcher/styles.tsx
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 export const DarkThemeSwitcherContainer = styled.div`
   display: flex;
+  justify-content: flex-end;
   align-items: center;
-  position: absolute;
-  top: 20px;
-  right: 20px;
+  position: relative;
+
   h4 {
     margin-right: 1ch;
   }
