c1a80ff242bcb72210c4e4ec656d575c23f685b2
Marcio Tiene <marciorft@gmail.com>
2020-12-16 17:12:47 -0300

    positioning darkTheme Switcher

--- src/components/DarkThemeSwitcher/styles.tsx
+++ src/components/DarkThemeSwitcher/styles.tsx
@@ -5,7 +5,8 @@ export const DarkThemeSwitcherContainer = styled.div`
   justify-content: flex-end;
   align-items: center;
   position: relative;
-
+  top: 10px;
+  margin-right: 2px;
   h4 {
     margin-right: 1ch;
   }
